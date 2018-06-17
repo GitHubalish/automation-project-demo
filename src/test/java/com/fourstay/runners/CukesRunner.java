39c4f5a5662fb38d13bfd5d0e1d46c7b715851b0
Fahrittin <fahrittin@Fahrittins-MBP.frontierlocal.net>
2018-06-17 17:37:36 -0500

    initial commit

---
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.fourstay.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = "html:target/cucumber-reports",
+		features = "src/test/resources/features", 
+		glue = "com/fourstay/step_definitions", 
+		tags = "@Staytest_125",
+		dryRun = true
+		
+		/* clean version
+		 * changed dryRun to true
+		 */
+		
+		// new push
+		)
+public class CukesRunner {
+
+}
